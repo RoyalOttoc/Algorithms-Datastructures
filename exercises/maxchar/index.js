e08a14636223e123d0703b4720969661c2a53813
Jong Kim <jong.kim8706@gmail.com>
2022-02-20 03:37:18 +1100

    add maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,26 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let charObj = {};
+    let max = 0;
+    let maxStr = '';
+
+    for (let char of str){
+        if(!charObj[char]){
+            charObj[char] = 1;
+        }else{
+            charObj[char]++;
+        }
+    }
+    for (let key in charObj){
+        if(charObj[key] > max){
+            max = charObj[key];
+            maxStr = key;
+        }
+    }
+    return maxStr
+
+}
 
 module.exports = maxChar;
