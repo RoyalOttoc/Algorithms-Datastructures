c82a249d372ff578c802e0d5244eea2a10c451c5
Jong Kim <jong.kim8706@gmail.com>
2022-02-20 03:28:51 +1100

    init commit

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,22 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+// function reverseInt(n) {
+//     numArray = n.toString().split('')
+//     if(numArray[0] === '-'){
+//         const withoutMinus = numArray.splice(1);
+//         const result = '-' + withoutMinus.reverse().join('')
+//         return Number.parseInt(result, 10)
+//     }
+//     return Number.parseInt(n.toString().split('').reverse().join(''))
+
+// }
+
+function reverseInt(n) {
+    const reversed = n.toString().split('').reverse().join('')
+
+    return Number.parseInt(reversed, 10) * Math.sign(n);
+    
+}
 
 module.exports = reverseInt;
--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,32 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// function reverse(str) {
+//     let strLen = str.length;
+//     let reversedStr = "";
+//     let num = 1
+//     while((strLen - num) >= 0){
+//         reversedStr += str[strLen - num];
+//         num++;
+//     }
+//     return reversedStr
+// }
+
+// function reverse(str){
+//     return str.split("").reverse().join("")
+// }
+
+// function reverse(str){
+//     let reversed = '';
+
+//     for (let character of str){
+//         reversed = character + reversed;
+//     }
+//     return reversed;
+// }
+
+function reverse(str){
+    return str.split('').reduce((reversed, char)=> char + reversed, '')
+}
 
 module.exports = reverse;
