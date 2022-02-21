016361db0619316dd7ee45632a6ad80f8ac0930d
Jong Kim <jong.kim8706@gmail.com>
2022-02-21 23:53:25 +1100

    add chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,18 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    const result = [];
+    let index = 0;
+    while(array.length > index){
+      
+        let a = array.slice(index, index+size)
+        result.push(a)
+        
+        index += size
+    
+    }
+    return result
+}
 
 module.exports = chunk;
