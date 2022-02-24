64f2f6dfb17a4fce5d73737046222f13abad72b8
Jong Kim <jong.kim8706@gmail.com>
2022-02-25 02:49:53 +1100

    add capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,37 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+// Solution 2
+function capitalize(str) {
+  let result = ""
+  for (let i = 0; i < str.length; i++) {
+    console.log(str[i]);
+    if(i === 0){
+      result += str[i].toUpperCase()
+    }
+    else if(str[i - 1] === " "){
+      result += str[i].toUpperCase()
+    } else {
+      result += str[i]
+    }
+    
+  }
+  return result
+}
 
 module.exports = capitalize;
+
+// Solution 1
+// function capitalize(str) {
+   
+//   let words = []
+//   for (let char of str.split(' ')){
+//     words.push(capFirst(char))
+//   }
+//   return words.join(' ')
+
+// }
+
+// function capFirst(str){
+//   return str[0].toUpperCase() + str.slice(1)
+// }
