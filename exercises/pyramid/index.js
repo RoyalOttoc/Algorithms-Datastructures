d515f443737d10dfffe1f51fb68205440b297729
Jong Kim <jong.kim8706@gmail.com>
2022-02-28 02:10:20 +1100

    add pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,42 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// solution2, recursive
+function pyramid(n, row = 0, level = "") {
+  if (row === n) {
+    return;
+  }
+
+  if (level.length === 2 * n - 1) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+
+  const midpoint = Math.floor((2 * n - 1) / 2);
+  let add = "";
+  if (midpoint - row <= level.length && midpoint + row >= level.length) {
+    add = "#";
+  } else {
+    add = " ";
+  }
+  pyramid(n, row, level + add);
+}
 
 module.exports = pyramid;
+
+// Solution1 2dArray
+// function pyramid(n) {
+//   const mid = Math.floor((n * 2 - 1) / 2);
+//   for (let row = 0; row < n; row++) {
+//     let level = "";
+
+//     for (let col = 0; col < 2 * n - 1; col++) {
+//       if (mid - row <= col && mid + row >= col) {
+//         level += "#";
+//       } else {
+//         level += " ";
+//       }
+//     }
+//     console.log(level);
+//   }
+// }
