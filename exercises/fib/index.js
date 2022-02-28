6ebfdce3b5cd409c8640e73f10f3743dafba86f3
Jong Kim <jong.kim8706@gmail.com>
2022-03-01 02:15:28 +1100

    add fibo

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,48 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function memoize(fn){
+    const cache = {};
+    return function(...args){
+        
+        if(cache[args]){
+            return cache[args];
+        }
+
+        const result = fn.apply(this, args);
+        cache[args] = result;
+
+        return result;
+    };
+}
+
+function slowFib(n) {
+  if (n < 2){
+      return n;
+  }
+
+  return fib(n-1) + fib(n-2)
+}
+
+const fib = memoize(slowFib);
+
+// solution2 - Recursive
+// function fib(n) {
+//   if (n < 2){
+//       return n;
+//   }
+
+//   return fib(n-1) + fib(n-2)
+// }
 
 module.exports = fib;
+
+// Solution 1 - iterate
+// function fib(n) {
+//   let result = [0, 1];
+//   for (let i = 2; i <= n; i++){
+//       result.push(result[i-1] + result[i-2])
+//     }
+  
+//   return result[n]
+// }
